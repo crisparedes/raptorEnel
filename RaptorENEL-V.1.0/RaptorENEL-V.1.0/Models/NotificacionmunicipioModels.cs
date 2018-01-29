e11aab447133dff007b8249295651ced26829c57
crisparedes <cristiankris93@gmail.com>
2018-01-29 10:47:36 -0600

    todo el proyecto

---
+++ RaptorENEL-V.1.0/RaptorENEL-V.1.0/Models/NotificacionmunicipioModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RaptorENEL_V._1._0.Models
+{
+    [Table("incidencias_notificacion_municipio", Schema = "public")]
+    public class Notificacionmunicipio
+    {
+        [Key]
+        public int id { get; set; }
+
+    }
+}
